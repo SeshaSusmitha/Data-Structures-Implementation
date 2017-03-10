a1757cd61830f9031601696482a8d16e93972d85
SeshaSusmitha <yellamraju.susmita@gmail.com>
2017-03-10 13:30:52 -0800

    Added inseeeert at end, search methods

--- single-linkedlist.cpp
+++ single-linkedlist.cpp
@@ -17,8 +17,10 @@ class single_llist
 
 	public:
 		node * create_node(int);
-		void insert(int); // Inserts element at the beginning of linked list
+		void insert_begin(int); // Inserts element at the beginning of linked list
+		void insert_end(int); //Inserts element at the end of linked list
 		void delete_val(int); //Deletes element if available from the linked list
+		void search(int); //Searches the list for the given element
 		void display(); //Displays all elements or a message if the list is empty
 
 		single_llist()
@@ -33,11 +35,13 @@ int main()
 	single_llist sl;
 	sl.display();
 	sl.delete_val(2);
-	sl.insert(2);
-	sl.insert(4);
-	sl.insert(6);
-	sl.insert(8);
+	sl.insert_begin(2);
+	sl.insert_begin(4);
+	sl.insert_end(6);
+	sl.insert_end(8);
 	sl.display();
+	sl.search(6);
+	sl.search(10);
 	sl.delete_val(1);
 	sl.delete_val(6);
 	sl.display();
@@ -67,7 +71,7 @@ node *single_llist :: create_node(int val)
 
 /* Inserts a new node in the linked list*/
 
-void single_llist :: insert(int val)
+void single_llist :: insert_begin(int val)
 {
 	struct node *temp, *temp1;
 	temp = create_node(val);
@@ -81,6 +85,30 @@ void single_llist :: insert(int val)
 		temp1 = head;
 		head = temp;
 		head->next = temp1;
+	}
+	cout<<"inserted ele "<<val<<endl;
+}
+
+void single_llist :: insert_end(int val)
+{
+	struct node *temp, *temp1;
+	temp = create_node(val);
+	if(head == NULL)
+	{
+		head = temp;
+		head->next = NULL;
+	}
+	else
+	{
+		temp1 = head;
+		// head = temp;
+		// head->next = temp1;
+		while(temp1->next != NULL)
+		{
+			temp1 = temp1->next;
+		}
+		temp1->next = temp;
+
 	}
 	cout<<"inserted ele "<<val<<endl;
 }
@@ -112,24 +140,24 @@ void single_llist :: delete_val(int val)
 {
 	struct node *temp, *temp1;
 	temp = head;
-	cout<<"val to be deleted is "<<val<<endl;
+	cout<<"\nval to be deleted is "<<val<<endl;
 	//cout<<"head value is "<<temp->data<<endl;
 	if(temp == NULL)
 	{
-		cout<<"List is empty\n";
+		cout<<"\nList is empty\n";
 		return;
 	}
 
 	else
 	{
-		while(temp != NULL)
+		while(temp->next != NULL)
 		{
-			if(temp ->data == val)
+			if(temp->next ->data == val)
 			{
 				
 				temp1 = temp->next;
-				head->next = temp1;
-				free(temp); //Frees the deleted value from memory
+				temp->next = temp1->next;
+				free(temp1); //Frees the deleted value from memory
 				return;
 			}
 			else
@@ -137,7 +165,7 @@ void single_llist :: delete_val(int val)
 				temp = temp->next;
 			}
 		}
-		cout<<"val to be deleted is not available in the list"<<endl;
+		cout<<"\nval to be deleted is not available in the list"<<endl;
 	}
 }
 
@@ -150,16 +178,36 @@ void single_llist :: display()
 	//cout<<"temp data "<<temp->data<<endl;
 	if(head == NULL)
 	{
-		cout<<"List is empty\n";
+		cout<<"\nList is empty\n";
 		return ;
 	}
-	cout<<"List of elements in the linked list :"<<endl;
+	cout<<"\nList of elements in the linked list :"<<endl;
 	while(temp != NULL)
 	{
 		cout<<temp->data;
 		cout<<"\n";
 		temp = temp ->next;
 	}
+}
+
+void single_llist ::search(int val)
+{
+
+	struct node *temp;
+	temp = head;
+	while(temp->next != NULL)
+	{
+		if(temp->data == val)
+		{
+			cout<<"\nElement found "<<endl;
+			return;
+		}
+		else
+		{
+			temp = temp ->next;
+		}
+	}
+	cout<<"\nElement to be searched not available in the list"<<endl;
 }
 
 
