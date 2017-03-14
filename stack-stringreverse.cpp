32ea2e59a7f713718b9d4191ec37154e5ba16113
SeshaSusmitha <yellamraju.susmita@gmail.com>
2017-03-13 21:15:36 -0700

    Added comments

--- stack-stringreverse.cpp
+++ stack-stringreverse.cpp
@@ -1,3 +1,8 @@
+/* Total Time Complexity = O(n)
+	Extra memory for stack as we are pushing all characters to stack.
+	Extra space will be proportional to number of characters.
+	So space complexity = O(n) */
+
 #include <iostream>
 #include <stack>
 using namespace std;
@@ -6,15 +11,18 @@ void string_reverse(char *c, int n)
 {
 
 	stack<char> s;
+
+	/* Time complexity is O(n) */
 	for(int i = 0; i < n; i++)
 	{
-		s.push(c[i]);
+		s.push(c[i]); /* Pushing characters to stack */
 	}
 
+	/* Time complexity is O(n) */
 	for(int i = 0; i < n; i++)
 	{
-		c[i] = s.top();
-		s.pop();  
+		c[i] = s.top();  /* adding top character to string */
+		s.pop();  		 /* Popping character form stack */
 	}
 
 }
