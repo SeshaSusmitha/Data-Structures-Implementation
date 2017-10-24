870135af52f12211a42391dd0b1e95910020d787
SeshaSusmitha <yellamraju.susmita@gmail.com>
2017-10-24 11:38:23 -0700

    Initial Commit

--- Arrays/string-compression.cpp
+++ Arrays/string-compression.cpp
@@ -1,54 +1,31 @@
-/*********************************************************************
-******************String Compression Implmenetation*******************
-*******************Cracking The Coding Interview Q 1.6 ***************
-*********************************************************************/
-
 #include <iostream>
 using namespace std;
 
-int main()
-{
-	string str = "aabccccaaa";
-	string new_str;
+int main(){
+	string s = "AAAABBBCC";
+	string new_str = "";
+	int len = s.length(), i = 1, count = 1;
+	if(len == 0){
+		cout << "zero length" << endl;
+		return 0;
+	}
+	if(len == 1){
+		s = s + '1';
+		cout << s << endl;
+		return 0;
+	}
+	while(i < len){
+		if(s[i] == s[i-1]){
+			count += 1;
 
-	char ch ;
-	char ch1;
-
-	int i = 0, count = 1, j = 1, total_count = 0, flag;
-	int length = str.length();
-	
-	
-	while(i < length)
-	{
-		ch = str[i];
-		cout << "ch = " << ch << endl;
-		count = 1;
-		j = i + 1;
-		while(j < length)
-		{	
-			if(str[j] == ch)
-			{
-				count++;
-				flag = 1;
-				j++;
-			}
-			else
-			{
-				flag = 0;
-				ch1 = count + '0';
-				new_str += ch;
-				new_str += ch1;
-				break;
-			}
 		}
-		i = i + count;
+		else{
+			new_str = new_str + s[i-1] + to_string(count);
+			count = 1;
+		}
+		i += 1;
 	}
-	if(flag == 1)
-	{
-		new_str += ch;
-		new_str += ch1;
-	}
-
-	cout<<"Compressed String = "<<new_str<<endl;
-
+	
+	new_str = new_str + s[i-1] + to_string(count);
+	cout << new_str << endl;
 }
